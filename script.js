57b71931b8971ce174f02dc6abefa7a07639ec78
Ganesh Nadivinti <ganeshnadivinti1436g@gmail.com>
2025-06-17 21:01:37 +0530

    Update script.js
    update

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
+
 let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
 
-let currenTime = new Date();
-
-hrs.innerHTML = currenTime.getHours;
-min.innerHTML = currenTime.getMinutes;
-sec.innerHTML = currenTime.getSeconds;
-
-setInterval(()=>{
+function updateClock() {
     let currenTime = new Date();
 
+    hrs.innerHTML = (currenTime.getHours() < 10 ? "0" : "") + currenTime.getHours();
+    min.innerHTML = (currenTime.getMinutes() < 10 ? "0" : "") + currenTime.getMinutes();
+    sec.innerHTML = (currenTime.getSeconds() < 10 ? "0" : "") + currenTime.getSeconds();
+}
 
-hrs.innerHTML = (currenTime.getHours()<10?"0":"")+currenTime.getHours();
-min.innerHTML =  (currenTime.getMinutes()<10?"0":"")+currenTime.getMinutes();
-sec.innerHTML =  (currenTime.getSeconds()<10?"0":"")+currenTime.getSeconds();
-},1000)
+updateClock();  
+
+setInterval(updateClock, 1000); 
+
+
+
